6de12b5a4f1d8e1e595ca210038dedccf5899181
nayeem-bd <52297907+nayeem-bd@users.noreply.github.com>
2020-10-11 21:31:32 +0600

    Initial commit

---
+++ app/src/main/java/com/nayeem_bd/chatapp/MainActivity.java
@@ -0,0 +1,111 @@
+package com.nayeem_bd.chatapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button addGroupButton;
+    private EditText addGroupEditText;
+    private ListView listView;
+    private ArrayList<String> groupList = new ArrayList<>();
+    private ArrayAdapter<String> arrayAdapter;
+    private String Name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addGroupButton = findViewById(R.id.addGroupButtonId);
+        addGroupEditText = findViewById(R.id.addGroupId);
+        listView = findViewById(R.id.listviewId);
+
+        arrayAdapter = new ArrayAdapter<String>(MainActivity.this,R.layout.listitem,R.id.listtextViewId,groupList);
+        listView.setAdapter(arrayAdapter);
+
+        request_username();
+
+        addGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                DatabaseReference databaseReference = firebaseDatabase.getReference(addGroupEditText.getText().toString());
+                databaseReference.setValue("");
+                addGroupEditText.setText("");
+                addGroupEditText.requestFocus();
+            }
+        });
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference(addGroupEditText.getText().toString());
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                groupList.clear();
+                for(DataSnapshot dataSnapshot1:dataSnapshot.getChildren()){
+                    groupList.add(dataSnapshot1.getKey().toString());
+
+                }
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, MessageActivity.class);
+                intent.putExtra("user_name",Name);
+                intent.putExtra("group_name",adapterView.getItemAtPosition(i).toString());
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void request_username() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Enter UserName");
+        final EditText inputName = new EditText(MainActivity.this);
+        builder.setView(inputName);
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Name = inputName.getText().toString().trim();
+                if(Name.isEmpty()){
+                    request_username();
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+
+                request_username();
+            }
+        });
+        builder.show();
+    }
+}
---
+++ app/src/main/java/com/nayeem_bd/chatapp/MessageActivity.java
@@ -0,0 +1,100 @@
+package com.nayeem_bd.chatapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class MessageActivity extends AppCompatActivity {
+    private TextView messageTextView;
+    private Button sendButton;
+    private EditText messageEditText;
+    DatabaseReference root;
+    String user_name,group_name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+        messageEditText = findViewById(R.id.messageEditTextId);
+        messageTextView = findViewById(R.id.messageTextViewId);
+        sendButton = findViewById(R.id.sendButtonId);
+
+        group_name = getIntent().getExtras().get("group_name").toString();
+        user_name = getIntent().getExtras().get("user_name").toString();
+
+        setTitle(group_name);
+
+        root = FirebaseDatabase.getInstance().getReference().child(group_name);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              //  Map<String ,Object> unique_key = new HashMap<String ,Object>();
+                String temp_key = root.push().getKey();
+               // root.updateChildren(unique_key);
+
+                Map<String ,Object> userMessage = new HashMap<String ,Object>();
+                DatabaseReference userRef = root.child(temp_key);
+                userMessage.put("name",user_name);
+                userMessage.put("message",messageEditText.getText().toString());
+                userRef.updateChildren(userMessage);
+
+                messageEditText.setText("");
+                messageEditText.requestFocus();
+            }
+        });
+        root.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                append_messages(dataSnapshot);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                append_messages(dataSnapshot);
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void append_messages(DataSnapshot dataSnapshot) {
+
+        Iterator i = dataSnapshot.getChildren().iterator();
+        while(i.hasNext()){
+            String Message = (String) ((DataSnapshot)i.next()).getValue();
+            String User_name = (String) ((DataSnapshot)i.next()).getValue();
+
+            messageTextView.append(User_name + " : "+ Message +"\n\n");
+        }
+    }
+}
